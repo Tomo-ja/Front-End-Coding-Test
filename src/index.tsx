0fc5255f87a6957a77fd96e10e2477b49b22a7e2
Tomo-ja <tomonao.hj@gmail.com>
2023-08-20 20:18:30 +0900

    apply theme and global css

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from 'styles/themes/defaultTheme';
+import { GlobalStyle } from 'styles/Global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
