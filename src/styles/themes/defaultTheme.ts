4f92ebfbc13df2379ab6e6116f372887465ac79f
Tomo-ja <tomonao.hj@gmail.com>
2023-08-20 19:40:48 +0900

    features: create default theme

---
+++ src/styles/themes/defaultTheme.ts
@@ -0,0 +1,25 @@
+export const defaultTheme = {
+  font: {
+    title: "24px",
+    body: "16px",
+    caption: "12px",
+    minimum: "10px",
+  },
+  palette: {
+    primary: "#4CB3F8",
+    text: {
+      regular: "#333333",
+      light: "#4D4D4D",
+      blue: "#4CB3F8",
+    },
+    button: {
+      primary: "#4CB3F8",
+      normal: "#B3B3B3",
+      secondary: "#4CB3F8",
+    },
+    background: {
+      light: "#F5F8FA",
+      dark: "#C8E6FA",
+    },
+  },
+};
