0b1158a1518c110015a2ce6ae8a248a498a9085f
Tomo-ja <tomonao.hj@gmail.com>
2023-08-21 21:25:16 +0900

    route all componet in App level

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from 'components/modules/navigation';
+import AppFooter from 'components/elements/AppFooter';
+import ContentWrapper from 'components/modules/contentWrapper';
+import NewContent from 'components/modules/newContent';
+
+import { AppStyled } from 'App.styled';
+import { MainStyled } from 'styles/components/Main.styled';
 
 function App() {
   return (
-    <div className="App">
-      test application
-    </div>
+    <AppStyled>
+      <Navigation />
+      <MainStyled>
+        <Routes>
+          <Route path='/' element={<NewContent />} />
+          <Route path='/article/:id' element={<ContentWrapper />} />
+        </Routes>
+        <AppFooter />
+      </MainStyled>
+    </AppStyled>
   );
 }
 
