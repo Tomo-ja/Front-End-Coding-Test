ae76d9b4ad9c3f6cf2f5ba59fd145e867c6a364a
Tomo-ja <tomonao.hj@gmail.com>
2023-08-21 21:05:44 +0900

    feature: add oncllick function on delete button

--- src/components/elements/buttons/buttonWithOnlyIcon/DeleteButton.tsx
+++ src/components/elements/buttons/buttonWithOnlyIcon/DeleteButton.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { ButtonWithOnlyIconProps } from './ButtonWithOnlyIcon.types'
 import { ButtonWithOnlyIconStyled } from './ButtonWithOnlyIcon.styled'
 import DeleteIcon from 'asset/icons/delete.svg'
 
-export const DeleteButton = () => {
+export const DeleteButton = ({ onClick }: ButtonWithOnlyIconProps) => {
+
+	const handleDeleteButtonClick = (event: React.MouseEvent) => {
+		event.stopPropagation()
+		onClick()
+	}
+
 	return (
-		<ButtonWithOnlyIconStyled>
+		<ButtonWithOnlyIconStyled onClick={handleDeleteButtonClick}>
 			<img src={DeleteIcon} alt="delete" />
 		</ButtonWithOnlyIconStyled>
 	)
